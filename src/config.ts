0578e98db94a53ca3d6563e531b9d7d3b0942bbf
estate-market-admin <105776319+JesseOSullivan@users.noreply.github.com>
2024-07-13 17:46:26 +1000

    s

--- src/config.ts
+++ src/config.ts
@@ -6,8 +6,8 @@ dotenv.config();
 
 export const bucketName = process.env.BUCKET_NAME
 export const YOUTUBE_API_KEY =  process.env.YOUTUBE_API_KEY
-export const accessKeyId = process.env.AWS_ACCESS_KEY_ID
-export const  secretAccessKey = process.env.S3_ACCESS_KEY
+export const accessKeyId = process.env.S3_ACCESS_KEY
+export const  secretAccessKey = process.env.S3_SECRET_KEY
 export const  region : string = 'ap-northeast-1'
   
   
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,9 @@ import express from 'express';
 import { processVideo, getSnippets } from './processVideo'; // Ensure the correct path
 import { bucketName } from './config';
 import cors from 'cors';
+import archiver from 'archiver';
+import { S3 } from 'aws-sdk';
+import { accessKeyId, secretAccessKey, region } from './config';
 
 const app = express();
 const port = 3001;
@@ -37,6 +40,47 @@ app.get('/get-snippets', async (req, res) => {
     res.send({ snippets: publicUrls });
   } catch (error: any) {
     res.status(500).send(`Error fetching snippets: ${error.message}`);
+  }
+});
+
+app.get('/download-all', async (req, res) => {
+  console.log('Downloading all snippets as a zip file...')
+  const videoUrl = req.query.url as string;
+  if (!videoUrl) {
+    return res.status(400).send('Missing URL parameter.');
+  }
+
+  try {
+    console.log('Fetching snippets...', accessKeyId, secretAccessKey, region)
+    const snippets = await getSnippets(videoUrl);
+    const s3 = new S3(
+      {
+        accessKeyId: accessKeyId,
+        secretAccessKey: secretAccessKey,
+        region: region
+      }
+    );
+
+    res.attachment('snippets.zip');
+    const archive = archiver('zip', { zlib: { level: 9 } });
+    archive.on('error', (err) => {
+      throw err;
+    });
+    archive.pipe(res);
+
+    for (const snippet of snippets) {
+      const s3Key = snippet.s3Key;
+      if (!bucketName) {
+        throw new Error('S3 bucket name is not provided.');
+      }
+
+      const stream = s3.getObject({ Bucket: bucketName, Key: s3Key }).createReadStream();
+      archive.append(stream, { name: `${snippet.title}.mp3` });
+    }
+
+    await archive.finalize();
+  } catch (error: any) {
+    res.status(500).send(`Error creating zip file: ${error.message}`);
   }
 });
 
