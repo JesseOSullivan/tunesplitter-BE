6d61e83e8fc62ae46a5749c03f3dec152cdc0a42
estate-market-admin <105776319+JesseOSullivan@users.noreply.github.com>
2024-07-14 15:28:07 +1000

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 import archiver from 'archiver';
-import { getSnippets, processVideo } from './processVideo'; // Ensure the correct path
+import { processVideo, getSnippets } from './processVideo'; // Ensure the correct path
 import { bucketName, accessKeyId, secretAccessKey, region } from './config';
 import { S3 } from 'aws-sdk';
-import cors from 'cors';
 
 const app = express();
 const port = 3001;
 
-// Enable CORS only in development mode
-if (process.env.NODE_ENV === 'development') {
-  app.use(cors()); // Enable CORS for all routes
-}
-
-app.use(express.json()); // To handle JSON body
+app.use(cors()); // Enable CORS for all routes
 
 // Middleware to log incoming requests
 app.use((req, res, next) => {
@@ -37,21 +32,21 @@ app.get('/process-and-fetch-snippets', async (req, res) => {
     }));
     res.send({ snippets: publicUrls });
   } catch (error: any) {
-    console.error(`Error processing video or fetching snippets: ${error.message}`);
     res.status(500).send(`Error processing video or fetching snippets: ${error.message}`);
   }
 });
 
 // Endpoint to download all snippets as a zip file
-app.post('/download-all', async (req, res) => {
+app.get('/download-all', async (req, res) => {
   console.log('Downloading all snippets as a zip file...');
-  const { snippets } = req.body;
-
-  if (!snippets || snippets.length === 0) {
-    return res.status(400).send('Missing snippets parameter.');
+  const videoUrl = req.query.url as string;
+  if (!videoUrl) {
+    return res.status(400).send('Missing URL parameter.');
   }
 
   try {
+    console.log('Fetching snippets...', accessKeyId, secretAccessKey, region);
+    const snippets = await getSnippets(videoUrl);
     const s3 = new S3({
       accessKeyId: accessKeyId,
       secretAccessKey: secretAccessKey,
@@ -65,18 +60,18 @@ app.post('/download-all', async (req, res) => {
     });
     archive.pipe(res);
 
-    if (!bucketName) {
-      throw new Error('S3 bucket name is not provided.');
-    }
     for (const snippet of snippets) {
-      const s3Key = snippet.url.split('.com/')[1];
+      const s3Key = snippet.s3Key;
+      if (!bucketName) {
+        throw new Error('S3 bucket name is not provided.');
+      }
+
       const stream = s3.getObject({ Bucket: bucketName, Key: s3Key }).createReadStream();
       archive.append(stream, { name: `${snippet.title}.mp3` });
     }
 
     await archive.finalize();
   } catch (error: any) {
-    console.error(`Error creating zip file: ${error.message}`);
     res.status(500).send(`Error creating zip file: ${error.message}`);
   }
 });
